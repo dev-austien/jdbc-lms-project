0881f9aa7c4605ed81e16059c6532d99a8bb6937
clncodertien <agangaustien@gmail.com>
2026-05-14 06:06:15 +0800

    added the Input helper for String, Int and Double. added the CRUDOperation for authentication: login and register

--- src/main/java/CRUDOperation.java
+++ src/main/java/CRUDOperation.java
@@ -1,4 +1,45 @@
+import java.sql.*;
+import java.util.*;
+
 public class CRUDOperation {
+
+    // --- Authentication --- //
+
+    public static boolean registerUser(String username, String password, String role ) {
+        String sql = "INSERT INTO Users (usernamme, password_hash, role) VALUES (?, ? ?)";
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+            pstmt.setString(3, role);
+
+            int rowAffected = pstmt.executeUpdate();
+            return rowAffected > 0;
+        } catch (SQLException e) {
+            System.out.println("Error registering user: " + e.getMessage());
+            return false;
+        }
+    }
+
+    public static boolean login(String username, String password) {
+        String sql = "SELECT * FROM Users WHERE username = ? AND password_hash = ?";
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+
+            ResultSet rs = pstmt.executeQuery();
+            return rs.next(); // Returns true if a match is found
+
+        } catch (SQLException e) {
+            System.out.println("Database error during login: " + e.getMessage());
+            return false;
+        }
+    }
+
+
 
 
 }
--- src/main/java/InputHelper.java
+++ src/main/java/InputHelper.java
@@ -1,3 +1,35 @@
+import java.util.*;
+
 public class InputHelper {
-    
+
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static String readString(String prompt) {
+        System.out.print(prompt);
+        return sc.nextLine().trim();
+    }
+
+    public static int readInt(String prompt) {
+        while (true) {
+            System.out.print(prompt);
+            try {
+                int input = Integer.parseInt(sc.nextLine().trim());
+                return input;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid whole number.");
+            }
+        }
+    }
+
+    public static double readDouble(double prompt) {
+        while (true) {
+            System.out.print(prompt);
+            try {
+                double input = Double.parseDouble(sc.nextLine().trim());
+                return input;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid decimal number.");
+            }
+        }
+    }
 }
